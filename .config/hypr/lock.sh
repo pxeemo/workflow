de68b98a33d5da93c46f8e619279a8fa06e03d14
pxeemo <shayans31516@gmail.com>
2023-10-26 19:31:52 +0330

    chore

--- .config/hypr/lock.sh
+++ .config/hypr/lock.sh
@@ -2,9 +2,10 @@
 
 case "$1" in
     immediate)
-        swaylock --screenshots --clock --effect-vignette 0.5:0.5 --separator-color fff000 --indicator-idle-visible --indicator-thickness 10 --inside-color 1e1e2ea5 --indicator-radius 100 --grace 1 --fade-in .3 --key-hl-color fff000 --line-color 00000000 --ring-color 893bff --effect-blur 7x5 --daemonize ;;
+        swaylock --screenshots --clock --effect-vignette 0.5:0.5 --separator-color fff000 --indicator-idle-visible --indicator-thickness 10 --inside-color 1e1e2ea5 --indicator-radius 100 --grace 1 --fade-in 0.3 --key-hl-color fff000 --line-color 00000000 --ring-color 893bff --effect-blur 7x5 --daemonize 
+        ;;
     delayed)
-        swaylock --screenshots --clock --effect-vignette 0.5:0.5 --separator-color fff000 --indicator-idle-visible --indicator-thickness 10 --inside-color 1e1e2ea5 --indicator-radius 100 --grace 8 --fade-in .3 --key-hl-color fff000 --line-color 00000000 --ring-color 893bff --effect-blur 7x5 --daemonize ;;
+        swaylock --screenshots --clock --effect-vignette 0.5:0.5 --separator-color fff000 --indicator-idle-visible --indicator-thickness 10 --inside-color 1e1e2ea5 --indicator-radius 100 --grace 8 --fade-in 0.3 --key-hl-color fff000 --line-color 00000000 --ring-color 893bff --effect-blur 7x5 --daemonize 
+        ;;
     *) echo default ;;
 esac
-
