97ec41ef2ac2997974a671a18165cf838a31f6cf
pxeemo <shayans31516@gmail.com>
2024-04-05 12:48:27 +0330

    switch to hypridle and hyprlock

--- .config/hypr/idle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-    timeout 300 '~/.config/hypr/lock.sh delayed' \
-    timeout 600 'hyprctl dispatch "dpms off"' \
-    timeout 1200 'hyprctl dispatch "dpms on"; systemctl suspend' \
-    before-sleep '~/.config/hypr/lock.sh immediate' \
-    lock '~/.config/hypr/lock.sh immediate'
