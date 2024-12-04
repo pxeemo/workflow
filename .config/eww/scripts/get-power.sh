e357df35a930ad1ba24a4d547a4b01d5f025de22
pxeemo <shayans31516@gmail.com>
2024-12-04 18:03:25 +0330

    chore
    Former-commit-id: d06c700f6436c6c62643a4e021453f2301db2cf0

--- .config/eww/scripts/get-power.sh
+++ .config/eww/scripts/get-power.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
 IS_CHARGING=
+IS_PLUGGED=
 PERC=
 
-get() {
+get_battery() {
     status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
     charging=$(echo "$status" | grep -ce 'state: *charging')
     percentage=$(echo "$status" | awk -F ' +|%' '/percentage/{print $3}')
     
     if [ "$charging" != "$IS_CHARGING" ] && [ -n "$IS_CHARGING" ]; then
-        if [ "$charging" == 1 ]; then
-            notify-send -i "../images/battery/1-99.svg" "Battery" \
-                "Battery is charging."
-        else
-            notify-send -i "../images/battery/0-60.svg" "Battery" \
-                "Battery is not charging."
+        if [ "$charging" != 1 ] && [ "$IS_PLUGGED" == "yes" ]; then
+            notify-send -i "$(realpath ../images/battery/1-99.svg)" "Battery" \
+                "Battery is full."
         fi
     fi
     IS_CHARGING="$charging"
 
     if [ "$percentage" -eq 25 ] && [ "$PERC" -gt "$percentage" ]; then
-            notify-send -i "../images/battery/0-20.svg" "Battery" \
-                "Battery is low. Please plug in the battey."
+        notify-send -i "$(realpath ../images/battery/0-20.svg)" "Battery" \
+            "Battery is low. Please plug in the battey."
     fi
     PERC="$percentage"
 
@@ -30,10 +28,30 @@ get() {
         '.charging = $charging | .percentage = ($percentage | tonumber)'
 }
 
+get_supply() {
+    status=$(upower -i /org/freedesktop/UPower/devices/line_power_AC)
+    plugged=$(echo "$status" | awk '/online/{print $2}')
+    if [ "$plugged" != "$IS_PLUGGED" ]; then
+        if [ "$plugged" == "yes" ]; then
+            notify-send -i "$(realpath ../images/battery/full.svg)" "Battery" \
+                "Power supply plugged in."
+        else
+            notify-send -i "$(realpath ../images/battery/0-60.svg)" "Battery" \
+                "Power supply disconnected."
+        fi
+    fi
+    IS_PLUGGED="$plugged"
+}
+
 # exit if upower fails to get battery
 upower -e | grep -q battery || exit 1
 
-get
-upower -m | while read; do
-    get
+cd $(dirname $(realpath $0))
+get_battery
+upower -m | while read -r line; do
+    if echo "$line" | grep -q "line_power_AC"; then
+        get_supply
+    else
+        get_battery
+    fi
 done
