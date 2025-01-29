810dbcbfab94e4514e60a912a6a6f876c308e883
pxeemo <shayans31516@gmail.com>
2025-01-29 17:04:16 +0330

    chore
    Former-commit-id: 2c56746e82e0b159f70d5453ec20e272c8ca4fae

--- .config/fish/functions/scred.fish
+++ .config/fish/functions/scred.fish
@@ -1,3 +1,8 @@
 function scred --wraps="which" --description 'open scripts in nvim'
-    $EDITOR (which $argv)
+    set script $HOME/.local/bin/$argv
+    $EDITOR $script
+
+    if test -f $script && test ! -x $script
+        chmod +x $script
+    end
 end
--- .local/bin/screentools.sh
+++ .local/bin/screentools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 option=("Active Window" "Select Area" "Text Scanner (OCR)" "QR Code Scanner"
-"Screen Record" "Colorpicker" "Magnifier")
+"Screen Record" "Colorpicker" "Magnifier" "Key Logger")
 
 choice=$(printf '%s\n' "${option[@]}" | wofi -dp "Screen tool")
 sleep .2
@@ -18,8 +18,19 @@ case "$choice" in
         text=$(grim -g "$(slurp)" - | zbarimg -)
         wl-copy "$text" && notify-send -a Scanner "Text copied" "$text"
         ;;
-    ${option[4]}) kitty -e fish -c screen-rec ;;
+    ${option[4]}) if ! pkill -2 -f wf-recorder; then screenrec; fi ;;
     ${option[5]}) hyprpicker -a ;;
-    ${option[6]}) hyprmag --radius 150 --scale 2 ;;
+    ${option[6]}) 
+        if [[ "$XDG_CURRENT_DESKTOP" == "niri" ]]; then
+            wooz
+        else
+            hyprmag --radius 150 --scale 2
+        fi
+        ;;
+    ${option[7]}) 
+        if ! pkill wshowkeys; then
+            wshowkeys -F "JetBrainsMono NF,22" -a bottom
+        fi
+        ;;
     *) exit ;;
 esac
