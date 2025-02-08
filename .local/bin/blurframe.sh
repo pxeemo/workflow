1a102d8a91e181d2e46a642d0f18b8c5db430d39
pxeemo <shayans31516@gmail.com>
2025-02-08 16:25:58 +0330

    update blurframe.sh
    Former-commit-id: d3d25b7041c1735938baba003dfeffa0e7a5113a

--- .local/bin/blurframe.sh
+++ .local/bin/blurframe.sh
@@ -2,18 +2,18 @@ file=$1
 geom=$(identify -format '%wx%h' -ping "$file")
 w="${geom%x*}"
 h="${geom#*x}"
-unit=$(printf "%.f" $(echo "($w+$h)/2*0.1" | bc))
-blur=$(printf "%.f" $(echo "$unit/2" | bc))
+unit=$(printf "%.f" $(echo "($w+$h)/2*0.1" | bc)) # frame size
+blur=$(printf "%.f" $(echo "$unit/2" | bc)) # blur radius
 [ $unit -gt 50 ] || unit=50
-shadow=$(printf "%.f" $(echo "$unit/6" | bc))
+shadow=$(printf "%.f" $(echo "$unit/4" | bc))
 newgeom=$(echo "$w+$unit" | bc)x$(echo "$h+$unit" | bc)
 
 magick "$file" \( +clone -background black -shadow "80x$shadow+0+0" \) \
     +swap -background none -layers merge +repage \
-    -gravity center -background '#ffffff22' -extent "$newgeom" \
+    -gravity center -background '#ffffff03' -extent "$newgeom" \
     \( \
         "$file" -resize 10% -gaussian-blur "10x$blur" \
         -resize "$newgeom^" -extent "$newgeom" \
     \) \
-    +swap -composite res.png
+    +swap -composite "${2:-out.png}"
 
