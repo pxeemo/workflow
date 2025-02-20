5c49b5073cf309fea8b9450154cc7ac4ea026795
pxeemo <shayans31516@gmail.com>
2025-02-21 01:27:05 +0330

    chore
    Former-commit-id: 500c4d773ee7589f2c999217d2cfd6c6fccd2446

--- .config/fish/functions/rem.fish
+++ .config/fish/functions/rem.fish
@@ -1,5 +1,6 @@
 function rem
     pacman -Q --color=always | \
         fzf --ansi --multi --preview 'paru -Qi {1}' --query "$argv" | \
+        sed -E -e 's/([^ ]+).*/\1/g' | \
         xargs -ro sudo paru -Rs
 end
---
+++ .config/fish/functions/rm.fish
@@ -0,0 +1,4 @@
+function rm --wraps=trash-put --description 'alias rm trash-put'
+  trash-put $argv
+        
+end
